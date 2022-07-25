011825690d5e9d6f650129b033e6f5828ec720ed
Mukul Mehta <mukul@workduck.io>
2022-07-25 19:43:25 +0530

    Change jest config to work with ESM

--- libs/mex-utils/src/mex-utils.ts
+++ libs/mex-utils/src/mex-utils.ts
@@ -1,3 +1,9 @@
+import { nanoid } from 'nanoid';
+
 export function mexUtils(): string {
   return 'mex-utils';
 }
+
+export const randomString = () => {
+  return `SNIPPET_${nanoid(5)}`;
+};
--- libs/mex-utils/tests/mex-utils.spec.ts
+++ libs/mex-utils/tests/mex-utils.spec.ts
@@ -1,7 +1,11 @@
-import { mexUtils } from '../src';
+import { mexUtils, randomString } from '../src';
 
 describe('mexUtils', () => {
   it('should work', () => {
     expect(mexUtils()).toEqual('mex-utils');
+  });
+
+  it('should work again I guess', () => {
+    expect(randomString()).toHaveLength(13);
   });
 });
