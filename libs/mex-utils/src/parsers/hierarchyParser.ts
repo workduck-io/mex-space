7acc3337bb9331b734e0a26e61aabb153ad9646d
Mukul Mehta <mukul@workduck.io>
2022-07-28 03:39:34 +0530

    Add strings, time and treeUtils helper functions

---
+++ libs/mex-utils/src/parsers/hierarchyParser.ts
@@ -0,0 +1,58 @@
+import { ILink } from '../types/core'
+
+export const hierarchyParser = (
+  linkData: string[],
+  options?: { withParentNodeId: boolean; allowDuplicates?: boolean }
+): ILink[] => {
+  const ilinks: ILink[] = []
+  const idPathMapping: { [key: string]: string } = {}
+  const pathIdMapping: { [key: string]: { nodeid: string; index: number } } = {}
+
+  for (const subTree of linkData) {
+    const nodes = subTree.split('#')
+
+    let prefix: string | undefined
+    let parentNodeId: string | undefined
+
+    if (nodes.length % 2 !== 0) throw new Error('Invalid Linkdata Input')
+
+    for (let index = 0; index < nodes.length; index += 2) {
+      const nodeTitle = nodes[index]
+      const nodeID = nodes[index + 1]
+
+      const nodePath = prefix ? `${prefix}.${nodeTitle}` : nodeTitle
+
+      /*
+            Drafts.A and Drafts.B exist, we need to check if the Drafts parent node is the same by checking
+            the parent nodeUID. This handles the case in which a nodeID might have two different node paths. 
+   
+            We still do not handle the case where there are 2 nodes with the same path but different Node IDs,
+            we handle that on the frontend for now
+          */
+
+      if (idPathMapping[nodeID]) {
+        if (idPathMapping[nodeID] !== nodePath) {
+          const ilinkAt = ilinks?.findIndex((ilink) => ilink.nodeid === nodeID)
+
+          if (ilinkAt) {
+            ilinks.splice(ilinkAt, 1, { ...ilinks[ilinkAt], path: nodePath })
+          }
+        }
+      } else if (pathIdMapping[nodePath] && !options?.allowDuplicates) {
+        // mog(`Found existing notePath: ${nodePath} with ${nodeID} at index: ${pathIdMapping[nodePath].index}`)
+        ilinks[pathIdMapping[nodePath].index] = { nodeid: nodeID, path: nodePath }
+      } else {
+        // mog(`Inserting: ${nodePath} with ${nodeID} at index: ${ilinks.length}`)
+        idPathMapping[nodeID] = nodePath
+        pathIdMapping[nodePath] = { nodeid: nodeID, index: ilinks.length }
+        const ilink: ILink = { nodeid: nodeID, path: nodePath }
+        ilinks.push(options?.withParentNodeId ? { ...ilink, parentNodeId } : ilink)
+      }
+
+      prefix = nodePath
+      parentNodeId = nodeID
+    }
+  }
+
+  return ilinks
+}
