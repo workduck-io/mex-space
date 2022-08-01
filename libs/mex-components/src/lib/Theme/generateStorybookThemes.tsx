48edf1f73959bb823016eb36dd113bc131dd8620
Mukul Mehta <mukul@workduck.io>
2022-08-01 23:10:56 +0530

    Remove circular dependency in generateStorybookThemes.tsx

--- libs/mex-components/src/lib/Theme/generateStorybookThemes.tsx
+++ libs/mex-components/src/lib/Theme/generateStorybookThemes.tsx
@@ -1,6 +1,6 @@
-import { defaultThemes } from '.';
+import { defaultThemes } from './Themes/defaultThemes'
 
 export const storybookThemes = defaultThemes.map((t) => ({
   name: t.id,
-  ...t.themeData,
-}));
+  ...t.themeData
+}))
