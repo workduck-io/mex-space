20317c8ce07da3debddaa1f4c759db1d7854647f
Mukul Mehta <mukul@workduck.io>
2022-08-04 02:35:47 +0530

    [MEX-UTILS] Add tests for flexsearch and fuzzysearch

---
+++ libs/mex-utils/tests/search/fuzzySearch.spec.ts
@@ -0,0 +1,24 @@
+import { test, expect } from 'vitest'
+
+import { fuzzySearch } from './../../src/search/fuzzySearch'
+
+test('Fuzzy Search - Valid Case 1', () => {
+  const list = ['Hullo', 'Wordl']
+  const results = fuzzySearch(list, 'Hello')
+  expect(results.length).toBeGreaterThan(0)
+})
+
+test('Fuzzy Search - Valid Case 2', () => {
+  const list = ['Hullo', 'Wordl']
+  const results = fuzzySearch(list, 'Batman')
+  expect(results.length).toBe(0)
+})
+
+test('Fuzzy Search - Valid Case 3', () => {
+  const list = [
+    { id: 1, text: 'Hullo' },
+    { id: 2, text: 'Wordl' }
+  ]
+  const results = fuzzySearch(list, 'Hello', (item) => item.text)
+  expect(results.length).toBeGreaterThan(0)
+})
