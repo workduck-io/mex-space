2b7fa5ac91a622afb096c1bb65ceb52aad14160c
xypnox <xypnox@gmail.com>
2022-12-02 23:02:26 +0530

    Fix style generator, add button style generator

--- libs/mex-themes/src/lib/styleGenerator.ts
+++ libs/mex-themes/src/lib/styleGenerator.ts
@@ -1,13 +1,14 @@
 import { css } from 'styled-components'
 
-import { BaseElementStyle, ElementStyle } from './types/theme'
+import { BaseElementStyle, ButtonStyle, CssVariableAccessor, ElementStyle } from './types/theme'
 
 export const generateStyleBase = <T>(bes: BaseElementStyle<T>) => {
-  const { surface, textColor, iconColor, border } = bes
+  const { surface, textColor, iconColor, border, borderLeft } = bes
   const style = {
     backgroundColor: surface,
     color: textColor,
-    border: border
+    border: border,
+    borderLeft: borderLeft
   }
 
   if (iconColor) {
@@ -40,3 +41,34 @@ export const cssStyle = (style: any) => {
     ${style}
   `
 }
+
+export const genButtonStyles = (style: ButtonStyle<CssVariableAccessor>) => {
+  return `
+    color: ${style.textColor};
+    background-color: ${style.surface};
+    svg,
+    iconify-icon {
+      color: ${style.iconColor};
+    }
+
+    &:hover {
+      background-color: ${style.hover.surface};
+      color: ${style.hover.textColor};
+      svg,
+      iconify-icon {
+        color: ${style.hover.iconColor};
+      }
+    }
+
+    &:active {
+      background-color: ${style.active.surface};
+      color: ${style.active.textColor};
+      svg,
+      iconify-icon {
+        color: ${style.active.iconColor};
+      }
+    }
+  `
+}
+
+// export const genButtonStylesLegacy = (style: ButtonStyle<CssVariableAccessor>) =>
