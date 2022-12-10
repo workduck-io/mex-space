33ec57de7d8512a0315881f4e2d431dcd5e78b44
xypnox <xypnox@gmail.com>
2022-12-10 16:25:27 +0530

    Use mex-themes in mex-components

--- libs/mex-components/src/lib/Theme/generateStorybookThemes.tsx
+++ libs/mex-components/src/lib/Theme/generateStorybookThemes.tsx
@@ -1,6 +1,29 @@
-import { defaultThemes } from './Themes/defaultThemes'
+import { defaultThemes, ManagedProvider } from '@workduck-io/mex-themes'
 
-export const storybookThemes = defaultThemes.map((t) => ({
-  name: t.id,
-  ...t.themeData
-}))
+export const storybookThemes = defaultThemes
+  .map((t) => {
+    return [
+      {
+        id: t.id,
+        name: t.name + ' dark',
+        data: t.data['dark']
+      },
+      {
+        id: t.id,
+        name: t.name + ' light',
+        data: t.data['light']
+      }
+    ]
+  })
+  .flat()
+
+export const providerFn = ({ theme, children }) => {
+  // console.log({ theme })
+  return (
+    <ManagedProvider legacySupport={false} globalInjection tokens={theme.data}>
+      {children}
+    </ManagedProvider>
+  )
+}
+
+// const themingDecorator = withThemes(null, defaultThemes, { providerFn })
